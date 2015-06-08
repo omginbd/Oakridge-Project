0733978d1560825ac834524e1dea8ee8ebfdd270
James Collier <jcollier@axis41.com>
2015-06-08 16:46:07 -0600

    styleStructure - added media queries to the main css file.  Completed the styling for the extra large media query size.  Adjusted the logo image to be transparent.

---
+++ resources/scripts/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#rightSide").css("width",($(document).width() - 350) + "px");
+});
+  
+$( window ).resize(function() {
+  $("#rightSide").css("width",($(document).width() - 350) + "px");
+});
+ 
