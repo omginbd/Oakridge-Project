42a97f9a3387054a31e1931dd776a01b40893e66
James Collier <jcollier@axis41.com>
2015-06-22 13:00:27 -0600

    cssAdjustments - restructured javascript to encapsulate better

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,101 +1,144 @@
 /*global $:false, jQuery:false */
 
-// function definitions
-var adjustSize,
-    mobileShow,
-    textResizer;
+// global vars
+var RESPONSIVE;
 
-// global variables
-var SMALL_BREAK_POINT = 768;
-
+/***********************************************************************************************************************
+* On Load
+*   - runs the responsive.adjustSize to accomodate current window size
+***********************************************************************************************************************/
 $(function () {
     'use strict';
-    adjustSize();
+    RESPONSIVE.adjustSize();
+    //RESPONSIVE.textResizer({"ele": ".title", "size": "full"});
 });
-  
+
+/***********************************************************************************************************************
+* On Resize
+*   - runs the responsive.adjustSize to accomodate current window size
+***********************************************************************************************************************/
 $(window).resize(function () {
     'use strict';
-    adjustSize();
-    textResizer({"ele": ".title", "size": "full"});
+    RESPONSIVE.adjustSize();
+    RESPONSIVE.textResizer({"ele": ".title", "size": "full"});
 });
 
-function adjustSize() {
+/***********************************************************************************************************************
+* RESPONSIVE
+*   - an app to hanldle window sizing.
+***********************************************************************************************************************/
+var RESPONSIVE = (function () {
     'use strict';
-    var windowWidth = $(window).width(),
-        navBarWidth = $(".navbar").width(),
-        rightSideEle = $("#rightSide");
-    
-    if (windowWidth < SMALL_BREAK_POINT) {
-        rightSideEle.css("width", windowWidth);
-        mobileShow(true);
-    } else {
-        rightSideEle.css("width", windowWidth - navBarWidth);
-        mobileShow(false);
-    }
-}
+    // responsive variables
+    var SMALL_BREAK_POINT = 768;
 
-function mobileShow(pShow) {
-    'use strict';
-    var mobileMenu = $("#mobileMenu"),
-        nav = $('nav'),
-        loginButton = $(".loginButton");
-    if (pShow) {
-        mobileMenu.show();
-        nav.hide();
-        loginButton.hide();
-    } else {
-        mobileMenu.hide();
-        nav.show();
-        loginButton.show();
+    /*******************************************************************************
+    * mobileShow()
+    *   - shows and hides neccessary compontents for mobile-sized useage
+    *******************************************************************************/
+    function mobileShow(pShow) {
+        var mobileMenu = $("#mobileMenu"),
+            nav = $('nav'),
+            loginButton = $(".loginButton");
+        if (pShow) {
+            mobileMenu.show();
+            nav.hide();
+            loginButton.hide();
+        } else {
+            mobileMenu.hide();
+            nav.show();
+            loginButton.show();
+        }
     }
-}
-
-function textResizer(params) {
-    'use strict';
-    var eleSelector = params.ele || "",
-        size = params.size || "",
-        parentWidth,
-        ele,
-        maxSize,
-        desiredSize,
-        findParentWidth = function () {
-            if (ele) {
-                parentWidth = ele.parent.width();
-            } else {
-                // no ele
-                return;
-            }
-        },
-        testForMaxSize = function () {
-            maxSize = 666;
-        };
     
-    if (eleSelector) {
-        ele = $(eleSelector);
-        if (ele) {
-            findParentWidth();
-            if (size && size === "full") {
-                testForMaxSize();
-                ele.css("font-size", maxSize + "px");
-            } else if (size && size === "half") {
-                testForMaxSize();
-                if (maxSize !== 0) {
-                    desiredSize = maxSize / 2;
+    /*******************************************************************************
+    * adjustSize()
+    *   - measures window width, and calls the appropriate functions to adjust the
+    *       view for the user
+    *******************************************************************************/
+    function adjustSize() {
+        var windowWidth = $(window).width(),
+            navBarWidth = $(".navbar").width(),
+            rightSideEle = $("#rightSide");
+
+        if (windowWidth < SMALL_BREAK_POINT) {
+            rightSideEle.css("width", windowWidth);
+            mobileShow(true);
+        } else {
+            rightSideEle.css("width", windowWidth - navBarWidth);
+            mobileShow(false);
+        }
+    }
+
+    /*******************************************************************************
+    * textResizer()
+    *   - accepts an element and desired size type, and scales text accordingly
+    *   - Size Types are:
+    *       full - largest size possible without going onto two lines
+    *       half - half of full
+    *   - future params:
+    *       breakSize - if it can't fit this specified size, allow two(multi) lines
+    *******************************************************************************/
+    function textResizer(params) {
+        var eleSelector = params.ele || "",
+            size = params.size || "",
+            parentWidth,
+            ele,
+            maxSize,
+            desiredSize,
+            findParentWidth = function () {
+                if (ele) {
+                    parentWidth = ele.parent().width();
                 } else {
-                    // maxWidth was 0
+                    // no ele
                     return;
                 }
-                ele.css("font-size", desiredSize + "px");
+            },
+            testForMaxSize = function () {
+                maxSize = 666;
+            };
+
+        if (eleSelector) {
+            ele = $(eleSelector);
+            if (ele) {
+                findParentWidth();
+                if (size && size === "full") {
+                    testForMaxSize();
+                    ele.css("font-size", maxSize + "px");
+                } else if (size && size === "half") {
+                    testForMaxSize();
+                    if (maxSize !== 0) {
+                        desiredSize = maxSize / 2;
+                    } else {
+                        // maxWidth was 0
+                        console.log("maxWidth was 0");
+                        return;
+                    }
+                    ele.css("font-size", desiredSize + "px");
+                } else {
+                    // no provided size
+                    console.log("no provided size");
+                    return;
+                }
             } else {
-                // no provided size
+                // couldn't find element by the provided selector
+                console.log("couldn't find element by the provided selector");
                 return;
             }
         } else {
-            // couldn't find element by the provided selector
+            // element selector empty
+            console.log("element selector empty");
             return;
         }
-    } else {
-        // element selector empty
-        return;
     }
-}
+    
+    // return the functions that need public access
+    return {
+        adjustSize: function () {
+            adjustSize();
+        },
+        textResizer: function (params) {
+            textResizer(params);
+        }
+    };
+}());
