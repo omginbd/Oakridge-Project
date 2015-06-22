39d14aa9e3b754a338515070005aa24ce4ac7b66
James Collier <jcollier@axis41.com>
2015-06-22 15:09:28 -0600

    cssAdjustments - some work to make the font size of the title adjust based on screen width

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,7 +1,8 @@
-/*global $:false, jQuery:false */
+/*global $:false, jQuery:false, devel: true */
 
 // global vars
-var RESPONSIVE;
+var RESPONSIVE,
+    ASSERT;
 
 /***********************************************************************************************************************
 * On Load
@@ -9,8 +10,14 @@ var RESPONSIVE;
 ***********************************************************************************************************************/
 $(function () {
     'use strict';
-    RESPONSIVE.adjustSize();
-    //RESPONSIVE.textResizer({"ele": ".title", "size": "full"});
+    try {
+        RESPONSIVE.adjustSize();
+        var x = 1;
+        ASSERT.assert(x === 1);
+    } catch (e) {
+        console.error(e);
+    }
+    
 });
 
 /***********************************************************************************************************************
@@ -20,7 +27,6 @@ $(function () {
 $(window).resize(function () {
     'use strict';
     RESPONSIVE.adjustSize();
-    RESPONSIVE.textResizer({"ele": ".title", "size": "full"});
 });
 
 /***********************************************************************************************************************
@@ -52,25 +58,6 @@ var RESPONSIVE = (function () {
     }
     
     /*******************************************************************************
-    * adjustSize()
-    *   - measures window width, and calls the appropriate functions to adjust the
-    *       view for the user
-    *******************************************************************************/
-    function adjustSize() {
-        var windowWidth = $(window).width(),
-            navBarWidth = $(".navbar").width(),
-            rightSideEle = $("#rightSide");
-
-        if (windowWidth < SMALL_BREAK_POINT) {
-            rightSideEle.css("width", windowWidth);
-            mobileShow(true);
-        } else {
-            rightSideEle.css("width", windowWidth - navBarWidth);
-            mobileShow(false);
-        }
-    }
-
-    /*******************************************************************************
     * textResizer()
     *   - accepts an element and desired size type, and scales text accordingly
     *   - Size Types are:
@@ -95,7 +82,44 @@ var RESPONSIVE = (function () {
                 }
             },
             testForMaxSize = function () {
-                maxSize = 666;
+                var testEle = ele.clone(),
+                    testEleHeight,
+                    testEleWidth = parseInt(ele.parent().css("width"), 10),
+                    testElePaddingLeft = parseInt(ele.parent().css("padding-left"), 10),
+                    testEleMarginLeft = parseInt(ele.parent().css("margin-left"), 10),
+                    testElePaddingRight = parseInt(ele.parent().css("padding-right"), 10),
+                    testEleMarginRight = parseInt(ele.parent().css("margin-right"), 10),
+                    previousTestEleHeight,
+                    done = false,
+                    currentSize = 1;
+                testEle.css("font-size", currentSize + "px");
+                testEle.css("position", "absolute");
+                testEle.css("z-index", "-1000"); // so the test element doesn't show
+                
+                testEle.attr("id", "fontResizeTestElement");
+                $("body").append(testEle);
+                testEle = $("#fontResizeTestElement");
+                testEle.css("width", testEleWidth - testEleMarginLeft - testEleMarginRight - testElePaddingLeft -
+                            testElePaddingRight + "px");
+                testEleHeight = testEle.height();
+                
+                previousTestEleHeight = testEleHeight;
+                while (!done) {
+                    testEle.css("font-size", currentSize + "px");
+                    testEleHeight = testEle.height();
+                    if ((testEleHeight / 2) > previousTestEleHeight) {
+                        done = true;
+                        currentSize -= 1;
+                        testEle.css("font-size", currentSize + "px");
+                    } else {
+                        currentSize += 1;
+                        previousTestEleHeight = testEleHeight;
+                    }
+                }
+                
+                testEle.remove();
+                
+                maxSize = currentSize;
             };
 
         if (eleSelector) {
@@ -111,24 +135,45 @@ var RESPONSIVE = (function () {
                         desiredSize = maxSize / 2;
                     } else {
                         // maxWidth was 0
-                        console.log("maxWidth was 0");
+//                        console.log("maxWidth was 0");
                         return;
                     }
                     ele.css("font-size", desiredSize + "px");
                 } else {
                     // no provided size
-                    console.log("no provided size");
+//                    console.log("no provided size");
                     return;
                 }
             } else {
                 // couldn't find element by the provided selector
-                console.log("couldn't find element by the provided selector");
+//                console.log("couldn't find element by the provided selector");
                 return;
             }
         } else {
             // element selector empty
-            console.log("element selector empty");
+//            console.log("element selector empty");
             return;
+        }
+    }
+    
+    /*******************************************************************************
+    * adjustSize()
+    *   - measures window width, and calls the appropriate functions to adjust the
+    *       view for the user
+    *******************************************************************************/
+    function adjustSize() {
+        var windowWidth = $(window).width(),
+            navBarWidth = $(".navbar").width(),
+            rightSideEle = $("#rightSide");
+
+        if (windowWidth < SMALL_BREAK_POINT) {
+            rightSideEle.css("width", windowWidth);
+            mobileShow(true);
+            textResizer({"ele": "#pageTitle", "size": "full"});
+        } else {
+            rightSideEle.css("width", windowWidth - navBarWidth);
+            mobileShow(false);
+            $("#pageTitle").css("font-size", ""); // use css font-size specs
         }
     }
     
@@ -139,6 +184,29 @@ var RESPONSIVE = (function () {
         },
         textResizer: function (params) {
             textResizer(params);
+        }
+    };
+}());
+
+/***********************************************************************************************************************
+* ASSERT
+*   - an app to simulate asserts.
+***********************************************************************************************************************/
+var ASSERT = (function () {
+    'use strict';
+    var assert = function (condition, message) {
+        if (!condition) {
+            message = message || "Assertion failed on: " + condition;
+            if (typeof Error !== "undefined") {
+                throw new Error(message); // use JavaScript's error object -- only supported in newer browsers
+            }
+            throw message; // fallback
+        }
+    };
+    
+    return {
+        assert: function (condition, message) {
+            assert(condition, message);
         }
     };
 }());
